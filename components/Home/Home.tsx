2aa38eca3195eb42c25431047eeb7ebb7d5467cb
Keerthi <rhkeerthi3@gmail.com>
2025-09-26 23:59:08 +0530

    Created new file

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import Recent from './Recent/Recent'
 
 function Home() {
   return (
     <div className='overflow-hidden h-[2000px]'>
       <Hero/>
+      <Recent/>
     </div>
   )
 }
