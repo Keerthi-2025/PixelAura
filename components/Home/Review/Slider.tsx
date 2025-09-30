cd92da99c36847a1691dad5c96573d0c95ae2c27
Keerthi <rhkeerthi3@gmail.com>
2025-09-30 19:26:21 +0530

    Create new file

--- components/Home/Review/Slider.tsx
+++ components/Home/Review/Slider.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
 
+"use client"
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import ReviewCard from './ReviewCard';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1324 },
+    items: 1,
+    slidesToSlide: 1 
+  },
+  tablet: {
+    breakpoint: { max: 1324, min: 764 },
+    items: 1,
+    slidesToSlide: 1 
+  },
+  mobile: {
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1 
+  }
+};
+
+
+
 function Slider() {
-  return (
-    <div>Slider</div>
-  )
+
+        <Carousel
+      arrows={true}
+      responsive={responsive}
+      ssr={true} // means to render carousel on server-side.
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={2000}
+      keyBoardControl={true}
+    >
+        <div>
+            <ReviewCard/>
+        </div>
+    </Carousel>
+  
 }
 
 export default Slider
