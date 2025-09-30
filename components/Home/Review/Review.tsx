3db187b640c7a7e80526200660f836e014a5f6d4
Keerthi <rhkeerthi3@gmail.com>
2025-09-30 19:21:00 +0530

    Create new Slider file under Review section

--- components/Home/Review/Review.tsx
+++ components/Home/Review/Review.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Slider from './Slider'
 
 function Review() {
   return (
-    <div>Review</div>
+    <div className='pt-16 pb-16'>
+        <div className='mt-16 w-[80%] mx-auto'>
+            
+            <Slider/>
+        </div>
+    </div>
   )
 }
 
