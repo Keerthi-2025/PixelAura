b07ade7aeedaebd21e08376d398579d221737239
Keerthi <rhkeerthi3@gmail.com>
2025-09-30 20:11:56 +0530

    Created new FAQCard file

--- components/Home/FAQ/FQA.tsx
+++ components/Home/FAQ/FQA.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
+import FAQCard from './FAQCard'
 
 function FQA() {
   return (
-    <div>FQA</div>
+    <div className='pt-16 pb-16 bg-gray-950'>
+        <div className='w-[80%] mx-auto mt-16 grid-cols-1 grid md:grid-cols-2 gap-10 items-center'>
+            <div>
+               <Image
+               src="/r1.jpg"
+               alt='FAQ Image'
+               height={500}
+               width={500}
+               />
+
+               <div>
+                <FAQCard/>
+               </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
